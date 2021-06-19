4ff3053bbb30f6fd141c700fa24c1a925d347819
hispanos <mailermartinez@gmail.com>
2021-06-19 16:55:59 -0500

    Component foods created

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Data from '../db/Data';
+import Food from '../components/foods/Food'
 
-const Foods = () => {
+const data = new Data();
 
-    return (
-        <h1>Estas son las guajolotas</h1>
-    )
+
+class Foods extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            foods: [],
+            categoryId: 1
+        }
+    }
+
+    componentDidMount() {
+        const getData = async () => {
+            const listFoods = await data.getFoodsByCategory(this.state.categoryId);
+            this.setState({ foods: listFoods })
+        }
+        getData();
+    }
+
+
+    render() {
+        return (
+            <>
+                {
+                    this.state.foods.map((food) => {
+                        return (
+                            <Food 
+                                key= {food.id}
+                                food= {food}
+                            />
+                        )
+                    })
+                }
+            </>
+        )
+    }
 
 }
+
 
 export default Foods;
