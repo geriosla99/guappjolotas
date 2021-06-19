5ae1784faa2feb026d3010111b167e642735c27a
hispanos <mailermartinez@gmail.com>
2021-06-19 17:16:50 -0500

    Updated the global styles

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { createGlobalStyle } from "styled-components";
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Foods from '../components/Foods';
 
+const GlobalStyle = createGlobalStyle`
+    body{
+        font-family: 'Inter', sans-serif;
+        margin: 0;
+        padding: 20px;
+        background: #F2F2F2;
+    }
+    
+`;
+
 const App = () => {
 
     return (
         <>
+            <GlobalStyle />
             <Header />
             <Search />
             <Categories />
