57c218f264baba6511586e4d09ee42dc8f5471a4
geriosla99 <geriosla@gmail.com>
2021-06-24 23:44:33 -0500

    header listo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import header from './header/header.css';
 
 const Header = () => {
     return (
-        <h1>Aquí el header</h1>
+        <div>
+            <span className="TopImages">
+                <img className="Logopages" src="https://i.imgur.com/7me5AZ2.png" alt="Logo de guappjolotas" />
+                <img className="ShoppingCar" src="https://i.imgur.com/TeEyXRv.png" alt="carrito" />
+            </span>
+                 <h1 className="TextPages">Nada como una 
+                    Guajolota para 
+                    empezar el día</h1>
+        </div>
+        
     )
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Logopages = styled.img`
+    width: 30%;
+    float: left;
+`
+
+const TextPages = styled.h1`
+    font-weight: bold;
+    font-size: 10vw;
+    margin-top: 25vw;
+`
+
+const ShoppingCar = styled.img`
+    float: right;
+    margin: 9vw;
+`
+
+const TopImages = styled.span`
+    margin: 5vw;
+    padding: 10vw;
+`
