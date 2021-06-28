aaf4e0099135ebae78d739a1f3a75876d0214ed5
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-27 19:26:44 -0500

    Header turned to responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,20 @@
-import React from 'react';
-import {Logopages, TextPages, ShoppingCar, TopImages} from './header/Header'
+import React from "react";
+import { Logopages, TextPages, ShoppingCar, TopImages } from "./header/Header";
 
 const Header = (props) => {
-    if (props.visible) {
-        return (
-            <div>
-                <TopImages>
-                    <Logopages 
-                        src="https://i.imgur.com/nSkGKJ3.png"
-                        alt="Logo de guappjolotas"
-                    />
-                    <ShoppingCar 
-                        src="https://i.imgur.com/TeEyXRv.png"
-                        alt="Carrito"
-                    />
-                </TopImages>
-                    <TextPages>Nada como una Guajolota para empezar el día</TextPages>
-            </div>
-            
-        )
-    }else {
-        return(null)
-    }
-
-}
+  if (props.visible) {
+    return (
+      <div>
+        <TopImages>
+          <Logopages src="https://i.imgur.com/nSkGKJ3.png" alt="Logo de guappjolotas" />
+          <ShoppingCar src="https://i.imgur.com/TeEyXRv.png" alt="Carrito" />
+        </TopImages>
+        <TextPages>Nada como una Guajolota para empezar el día</TextPages>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
 
 export default Header;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,37 @@
 import styled from 'styled-components'
 
 export const Logopages = styled.img`
-    width: 64px;
-    height: 64px
-`
+    width: 62.81px;
+    height: 54.18px;
+`;
 
 export const TextPages = styled.h1`
     font-weight: bold;
     font-size: 34px;
-`
+    @media (min-width: 500px) {
+    text-align: center;
+    max-width: 600px;
+    margin-left: auto;
+    margin-right: auto;
+  } 
+`;
 
 export const ShoppingCar = styled.img`
     width: 24px;
     height: 24px;
-`
+    @media (min-width: 768px) {
+    width: 80px;
+    height: auto;
+  } 
+`;
 
 export const TopImages = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-`
+    @media (min-width: 768px) {
+    margin-left: 30px;
+    margin-right: 30px;
+  } 
+`;
