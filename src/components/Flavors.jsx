6b914ad2fef049ee870468a116452294e0b1b97f
geriosla99 <geriosla@gmail.com>
2021-07-06 19:44:23 -0500

    flavor-clean

--- src/components/Flavors.jsx
+++ src/components/Flavors.jsx
@@ -54,32 +54,3 @@ const Flavors = ({foods, selectFood, isLoaded}) => {
 }
 
 export default Flavors
-
-
-
-//     export default class Flavor extends Component {
-
-//         constructor(props){
-//             super(props);
-//             this.state={
-//                 flavor:[]
-//             }
-//         }
-
-//     handleClick(){
-//         console.log('estamos intentando conseguir los flavors')
-//     }
-    
-//     render() {
-//         return (
-//             <div className="content">
-//                 <div onClick={this.handleClick} className="Flavor-position verde">verde</div>
-//                 <div onClick={this.handleClick} className="Flavor-position mole">mole</div>
-//                 <div onClick={this.handleClick} className="Flavor-position raja">raja</div>
-//                 <div onClick={this.handleClick} className="Flavor-position piña">piña</div>
-//                 <div onClick={this.handleClick} className="Flavor-position pasas">pasas</div>
-//                 <div onClick={this.handleClick} className="Flavor-position guayaba">guayaba</div>
-//             </div>
-//         )
-//     }
-// }
