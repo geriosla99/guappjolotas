6b2179d164640229ad9ea52ebfc7c02dfcdbea5f
Malena Ramirez <maramirezuribe@gmail.com>
2021-07-08 17:42:41 -0500

    Code improvements

--- src/components/ButtonAddCar.jsx
+++ src/components/ButtonAddCar.jsx
@@ -1,79 +1,52 @@
-import React from 'react'
-import { ButtonAdd, DivButtonAdd } from './Back-Carshop/ButtonsBsStyles'
+import React from 'react';
+import { ButtonAdd, DivButtonAdd } from './Back-Carshop/ButtonsBsStyles';
 import { useHistory } from 'react-router';
 
 const ButtonsAddCar = ({ temporalCart }) => {
+  const currentCart = JSON.parse(localStorage.getItem('cart'));
+  const history = useHistory();
 
-   const currentCart = JSON.parse(localStorage.getItem('cart'));
-   
-   const history = useHistory();
+  const handleClick = () => {
+    let cart = {
+      items: [
+        {
+          ...temporalCart.item,
+          quantity: temporalCart.quantity,
+          subtotal: temporalCart.item.price * temporalCart.quantity,
+        },
+      ],
+      quantityItems: temporalCart.quantity,
+      total: temporalCart.subtotal,
+    };
 
-   const handleClick = () => {
+    if (temporalCart.additions.price) {
+      cart.items = [
+        ...cart.items,
+        {
+          ...temporalCart.additions,
+          quantity: 1,
+          subtotal: temporalCart.additions.price,
+        },
+      ];
+      cart.quantityItems += 1;
+    }
 
-      let cart = {
-         items: [
-            {
-               ...temporalCart.item,
-               quantity: temporalCart.quantity,
-               subtotal: temporalCart.item.price * temporalCart.quantity
-            }
-         ]
-      }
-      if (temporalCart.additions.price) {
-         cart.items = [
-            ...cart.items,
-            {
-               ...temporalCart.additions,
-               quantity: 1,
-               subtotal: temporalCart.additions.price
-            }
-         ]
-      }
+    if (currentCart) {
+      cart.quantityItems += currentCart.quantityItems;
+      cart.total += currentCart.total;
+    }
 
-      // let cart = {}
-      // if (temporalCart.additions.price) {
-      //    cart = {
-      //       items: [
-      //          temporalCart.item,
-      //          temporalCart.additions
-      //       ]
-      //    }
-      //    cart.items[1].quantity = 1
-      //    cart.items[1].subtotal = temporalCart.additions.price
-      //    cart.items[0].quantity = temporalCart.quantity
-      //    cart.items[0].subtotal = temporalCart.item.price * temporalCart.quantity
+    localStorage.setItem('cart', JSON.stringify(cart));
+    history.push('/cart');
+  };
 
-      // } else {
-      //    cart = {
-      //       items: [
-      //          temporalCart.item
-      //       ]
-      //    }
-      //    cart.items[0].quantity = temporalCart.quantity
-      //    cart.items[0].subtotal = temporalCart.item.price * temporalCart.quantity
-      // }
+  return (
+    <DivButtonAdd>
+      <ButtonAdd onClick={handleClick}>
+        Agregar {temporalCart.quantity} al carrito <div>${temporalCart.subtotal}.00</div>
+      </ButtonAdd>
+    </DivButtonAdd>
+  );
+};
 
-
-      cart.total = currentCart ? currentCart.total + temporalCart.subtotal : temporalCart.subtotal;
-
-
-      if (currentCart) {
-         cart.quantityItems = currentCart.quantityItems + temporalCart.additions.price ? temporalCart.quantity + 1 : temporalCart.quantity;
-      } else {
-        cart.quantityItems = temporalCart.additions.price ? temporalCart.quantity + 1 : temporalCart.quantity;
-      }
-      console.log(cart);
-      localStorage.setItem('cart', JSON.stringify(cart))
-      history.push('/cart')
-   }
-
-   return (
-      <DivButtonAdd >
-         <ButtonAdd onClick={handleClick}>Agregar {temporalCart.quantity} al carrito  <div>${temporalCart.subtotal}.00</div>
-         </ButtonAdd>
-      </DivButtonAdd>
-   )
-
-}
-
-export default ButtonsAddCar
+export default ButtonsAddCar;
